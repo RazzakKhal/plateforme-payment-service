7789c68fd7761358420f22bb2609d208b1be1f8d
razzak <khalfallah.razzak@gmail.com>
2025-05-13 03:54:41 +0200

    add var test

--- src/test/java/com/bookNDrive/payment_service/PaymentServiceApplicationTests.java
+++ src/test/java/com/bookNDrive/payment_service/PaymentServiceApplicationTests.java
@@ -1,9 +1,12 @@
 package com.bookNDrive.payment_service;
 
+import com.bookNDrive.payment_service.configuration.MoneticoProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(MoneticoProperties.class)
 class PaymentServiceApplicationTests {
 
 	@Test
