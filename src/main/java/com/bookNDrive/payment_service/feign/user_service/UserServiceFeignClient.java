87594cff2c9a200cde0b0d3584dbe3254f5ae73b
razzak <khalfallah.razzak@gmail.com>
2025-05-23 18:51:14 +0200

    make put instead of patch because feign doesn support patch

--- src/main/java/com/bookNDrive/payment_service/feign/user_service/UserServiceFeignClient.java
+++ src/main/java/com/bookNDrive/payment_service/feign/user_service/UserServiceFeignClient.java
@@ -3,10 +3,7 @@ package com.bookNDrive.payment_service.feign.user_service;
 import com.bookNDrive.payment_service.feign.user_service.dtos.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient("user-service")
 public interface UserServiceFeignClient {
@@ -15,6 +12,6 @@ public interface UserServiceFeignClient {
     @GetMapping("/users/me")
     public ResponseEntity<UserDto> getUser(@RequestHeader("Authorization") String bearerToken);
 
-    @PatchMapping("/users/formula")
-    void updateUserFormula(@RequestParam("formulaId") Long formulaId, @RequestHeader("Authorization") String bearerToken);
+    @PutMapping("/users/formula")
+    public ResponseEntity<Void> updateUserFormula(@RequestParam("formulaId") Long formulaId, @RequestHeader("Authorization") String bearerToken);
 }
